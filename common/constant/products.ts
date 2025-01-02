d414c374c4785b4222a90f28f7c89d7ac1ffe63f
Muhammad Rafi Ali <therafiali@gmail.com>
2025-01-03 01:05:17 +0500

    first commit

---
+++ common/constant/products.ts
@@ -0,0 +1,104 @@
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        name: "Canon camera",
+        price: 89,
+        image: "/assets/img/camera-1.png",
+        category: "Cameras",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 4.5,
+        featured: true
+    },
+    {
+        id: 2,
+        name: "Dell Server R730",
+        price: 259999,
+        image: '/assets/cisco-switch.jpg',
+        category: "Accessories",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 3.5,
+        featured: false
+    },
+    {
+        id: 3,
+        name: "Cisco POE Switch",
+        price: 85000,
+        image: '/assets/cisco-switch.jpg',
+        category: "Electronics",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 5,
+        featured: false
+    },
+    {
+        id: 4,
+        name: "10G SFP CARD",
+        price: 2279,
+        image: '/assets/sfp.avif',
+        category: "Mouse",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 4.5,
+        featured: false
+    },
+    {
+        id: 5,
+        name: "Dell Server R730",
+        price: 499,
+        image: '../assets/dell-server.png',
+        category: "Tablets",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 3.5,
+        featured: false
+    },
+    {
+        id: 6,
+        name: "Cisco POE Switch",
+        price: 999,
+        image: '/assets/cisco-switch.jpg',
+        category: "Computers",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 4.5,
+        featured: false
+    },
+    {
+        id: 7,
+        name: "10G SFP CARD",
+        price: 299,
+        image: '/assets/sfp.avif',
+        category: "Monitors",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 5,
+        featured: false
+    },
+    {
+        id: 8,
+        name: "Cisco POE Switch",
+        price: 129,
+        image: '/assets/cisco-switch.jpg',
+        category: "Keyboards",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 4,
+        featured: false
+    },
+    {
+        id: 9,
+        name: "Cisco POE Switch",
+        price: 39,
+        image: '/assets/cisco-switch.jpg',
+        category: "Accessories",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 3.5,
+        featured: false
+    },
+    {
+        id: 10,
+        name: "10G SFP CARD",
+        price: 79,
+        image: '/assets/sfp.avif',
+        category: "Speakers",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quibusdam.",
+        rating: 4.5,
+        featured: false
+    }
+]
---
+++ common/types/products.ts
@@ -0,0 +1,16 @@
+import { StaticImageData } from "next/image"
+
+export interface IProduct {
+    id: number
+    name: string
+    price: number
+    image: string 
+    category: string
+    description: string
+    rating: number
+    featured: boolean
+}
+
+export interface ProductItemProps {
+    product: IProduct[]
+}
